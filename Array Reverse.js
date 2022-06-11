be0ab65d1394642c56012b1d7b9db19020ccddf5
Fashad Ahmed <fashad.ahmed20@gmail.com>
2022-06-12 01:13:29 +0500

    Hackerrank

---
+++ Array Reverse.js
@@ -0,0 +1,10 @@
+function reverseArr(input) {
+    var ret = new Array;
+    for(var i = input.length-1; i >= 0; i--) {
+        ret.push(input[i]);
+    }
+    return ret;
+}
+
+var a = [3,5,7,8]
+var b = reverseArr(a);
