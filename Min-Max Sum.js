79278c15d2a5401b94ae6c8389cf155486e436e9
Fashad Ahmed <fashad.ahmed20@gmail.com>
2022-06-18 19:59:13 +0500

    Hackerrank

---
+++ Min-Max Sum.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let arr1 = arr.sort()
+    let min = 0
+    let max = 0
+    for (let i=0; i<arr1.length-1; i++){
+        min += arr1[i]
+    }
+    for (let j=1; j<arr1.length; j++){
+        max += arr1[j]
+    }
+    console.log(min, max)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
