87904240504dda1114e6ee9c939f77b0ffe29219
Fashad Ahmed <fashad.ahmed20@gmail.com>
2022-06-18 20:27:32 +0500

    Hackerrank

---
+++ Birthday Cake Candles.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let len = candles.length
+    let max = candles[0]
+    let counter=0
+    for(let i=0; i<len; i++){
+        if(max < candles[i]){
+            max = candles[i]
+        }
+    }
+    for(let j=0; j<len; j++){
+        if(max == candles[j]){
+            counter += 1
+        }
+    }
+    return counter
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
