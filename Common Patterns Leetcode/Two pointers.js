3767f870f99b129402809ce7fc9f24dd335396b1
Fashad Ahmed <fashad.ahmed20@gmail.com>
2023-06-22 20:48:20 +0500

    Create Two pointers.js
    Here are code templates for common patterns for all the data structures and algorithms looked at in LICC.

---
+++ Common Patterns Leetcode/Two pointers.js
@@ -0,0 +1,16 @@
+// code templates for common patterns for all the data structures and algorithms looked at in LICC.
+
+let fn = arr => {
+    let left = 0, ans = 0, right = arr.length - 1;
+
+    while (left < right) {
+        // do some logic here with left and right
+        if (CONDITION) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return ans;
+}
