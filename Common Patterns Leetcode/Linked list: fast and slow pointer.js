647f99d34895bee430a16c1be0c22f4aa6cc7010
Fashad Ahmed <fashad.ahmed20@gmail.com>
2023-06-22 20:53:05 +0500

    Create Linked list: fast and slow pointer.js

---
+++ Common Patterns Leetcode/Linked list: fast and slow pointer.js
@@ -0,0 +1,13 @@
+let fn = head => {
+    let slow = head;
+    let fast = head;
+    let ans = 0;
+    
+    while (fast && fast.next) {
+        // do logic
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return ans;
+}
