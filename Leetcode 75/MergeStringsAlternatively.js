ec3366803cb36000e7737200ab243f2500843eaa
Fashad Ahmed <fashad.ahmed20@gmail.com>
2023-06-24 21:46:44 +0500

    add: Day 01 : 1/75 🐱‍💻

---
+++ Leetcode 75/MergeStringsAlternatively.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+    let result = "";
+    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        if (i < word1.length) {
+            result += word1[i];
+        }
+        if (i < word2.length) {
+            result += word2[i];
+        }
+    }
+    return result;
+
+};
