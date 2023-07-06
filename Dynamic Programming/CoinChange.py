0194eabbce88f93f88f641549ee035406769ab73
Fashad Ahmed <fashad.ahmed20@gmail.com>
2023-07-06 11:39:17 +0500

    dp[j] += dp[j - c[i]]

---
+++ Dynamic Programming/CoinChange.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getWays' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. LONG_INTEGER_ARRAY c
+#
+
+def getWays(n, c):
+    # Write your code here
+    dp = [0] * (n+1)
+    
+    #base case
+    dp[0] = 1
+    
+    #main logic
+    for i in range(len(c)):
+        for j in range(c[i], n+1):
+            dp[j] += dp[j - c[i]]
+        
+        print(dp, c[i])
+    return dp[n]
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    ways = getWays(n, c)
+
+    fptr.write(str(ways) + '\n')
+
+    fptr.close()
